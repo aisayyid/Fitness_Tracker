acb66a9981857669038c05d0457e7e1e489c59a3
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-08-03 00:42:24 -0400

    created a routes folder and added api routes needed.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const Workout = require("../models");
+
+// You will need at least Four (4) api routes:
+// - GET ( "/api/workouts" ) - Get all workouts
+// - You will also need to figure out how to calculate the total duration from all exercises inside the exercises Array.
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+// - GET ( "/api/workouts/range" ) - Get the first 7 Workouts from the Workout table to be used in the "stats" page.
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+// - POST ( "/api/workouts" ) - Create a new Workout
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+// - PUT ( "/api/workouts/:id" ) - Update a Workout
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+module.exports = router;
